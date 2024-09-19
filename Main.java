0d2940e30a68bbf29601c568604ec7b310181d85
Valeriya <valera.dudko.25@gmail.com>
2024-09-19 16:24:40 +0300

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is first stpweb lab.");
+        System.out.println("This is first stpweb lab. I forked it, now i write in newBranch.");
     }
 }
